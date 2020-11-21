cf8ebd4a4389738905b9d354e55c1d69420a6667
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-11-21 14:04:30 -0500

    Initial commit: React app with webpack, babel, & eslint

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'Midwoofery CMS front end!';
+
+ReactDOM.render(<App title={title} />, document.getElementById('app'));
+
+module.hot.accept();
