836524242cce15591fe21b2496d40b2173f14773
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-11-21 15:56:53 -0500

    Implement test call to dev API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const App = ({ title }) => <div>{title}</div>;
+const App = ({ title }) => {
+  const [dogs, setDogs] = useState([]);
+
+  const loadDogs = async () => {
+    const res = await fetch(`${process.env.API_BASE}/dogs`);
+    const data = await res.json();
+    setDogs(data);
+  };
+
+  useEffect(() => {
+    loadDogs();
+  }, []);
+
+  const dogList = dogs.map((d) => <li key={d.id}>{d.name}</li>);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>{dogList}</ul>
+    </div>
+  );
+};
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
