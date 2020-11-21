3c84943f7b78c23ef01be2dda7a4c2969c571f53
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-11-21 15:18:38 -0500

    Add .env files

--- build-tools/webpack.dev.js
+++ build-tools/webpack.dev.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new Dotenv({
+      path: path.resolve(__dirname, '..', `./.env.dev`),
+    }),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, '..', './dist'),
     hot: true,
--- build-tools/webpack.prod.js
+++ build-tools/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'production',
@@ -9,6 +10,9 @@ module.exports = {
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
     new CleanWebpackPlugin(),
+    new Dotenv({
+      path: path.resolve(__dirname, '..', `./.env.prod`),
+    }),
   ],
   devtool: 'source-map',
 };
